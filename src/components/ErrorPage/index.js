471cd0e92bfb417b597e87e0e14a6329dc68a3dc
Thomas Fouquert <tfouquert@gmail.com>
2022-09-03 16:33:50 +0200

    merge

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import batman from '../../images/batman.png'
 
 const ErrorPage = () => {
   return (
     <div className="quiz-bg">
       <div className="container">
-        ErrorPage
+        <h2 className="text-center">Wrong Page...</h2>
+
+        <img src={batman} alt="error page"/>
       </div>
     </div>
   )
