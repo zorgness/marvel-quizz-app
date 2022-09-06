0b9687ff5588f6ddcd37a9a34b5847a12dbac9cc
Thomas Fouquert <tfouquert@gmail.com>
2022-09-06 18:29:28 +0200

    validate_answer

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -12,18 +12,54 @@ class Quiz extends Component {
     storedQuestion: [],
     question: null,
     options: [],
-    idQuestion: 0
+    idQuestion: 0,
+    btnDisabled: true,
+    userAnswer: null,
+    score: 0
   }
+
+  storedQuestionRef = React.createRef();
 
   loadQuestions = level =>  {
     const fetchArrayQuiz = QuizMarvel[0].quizz[level];
+
     if(fetchArrayQuiz.length >= this.state.maxQuestion ) {
+
+      this.storedQuestionRef.current = fetchArrayQuiz;
       const newArray = fetchArrayQuiz.map(({answer, ...rest}) => rest )
+
+      // const destructuredArray = fetchArrayQuiz.reduce((acc, val) => {
+      //   Object.keys(val).forEach(key => (acc[key] = acc[key] || []).push(val[key]));
+      //   return acc;
+      // }, {});
 
       this.setState({storedQuestion: newArray})
 
     } else {
       console.log("not enough questions loaded")
+    }
+  }
+
+  submitAnswer = (option) => {
+
+    this.setState({userAnswer: option, btnDisabled: false})
+
+  }
+
+  nextQuestion = () => {
+    if(this.state.idQuestion === this.state.maxQuestion - 1) {
+      console.log("maxQuestion reached");
+    } else {
+      this.setState(prevState => ({
+        idQuestion: prevState.idQuestion + 1
+      }))
+    }
+
+    const goodAnswer = this.storedQuestionRef.current[this.state.idQuestion].answer;
+    if(this.state.userAnswer === goodAnswer) {
+      this.setState(prevState => ({
+        score: prevState.score + 1
+      }))
     }
   }
 
@@ -38,7 +74,7 @@ class Quiz extends Component {
 
       this.setState({
         question: this.state.storedQuestion[this.state.idQuestion].question,
-        options : this.state.storedQuestion[this.state.idQuestion].options
+        options : this.state.storedQuestion[this.state.idQuestion].options,
       })
 
     }
@@ -50,7 +86,10 @@ class Quiz extends Component {
 
     const displayOption = this.state.options.map((option, index) => {
       return (
-        <p key={index} className="answerOptions">{option}</p>
+        <p key={index}
+        onClick={() => this.submitAnswer(option)}
+        className={`answerOptions ${this.state.userAnswer === option ? "selected" : null}`}>
+        {option}</p>
       )
     })
 
@@ -66,7 +105,7 @@ class Quiz extends Component {
 
         {displayOption}
 
-      <button className="btnSubmit">Next</button>
+      <button disabled={this.state.btnDisabled} onClick={this.nextQuestion} className="btnSubmit">Next</button>
     </div>
   )
 
