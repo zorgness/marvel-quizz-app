cde5c6fd6e7cf53ad0901cf8d0d84b42d24c9c63
Thomas Fouquert <tfouquert@gmail.com>
2022-09-07 13:18:47 +0200

    quiz_over

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 
-const QuizOver = () => {
+const QuizOver = React.forwardRef((props, ref) => {
+
+  console.log(ref)
   return (
-    <div>QuizOver</div>
+    <Fragment>
+      <div className="stepsBtnContainer">
+        <p className="successMsg">Success, go on next level!</p>
+        <button className="btnResult success">Next level</button>
+      </div>
+
+      <div className="percentage">
+        <div className="progressPercent">Success:</div>
+        <div className="progressPercent">Score:</div>
+      </div>
+
+      <hr/>
+
+      <p>Rigth Answer</p>
+
+    </Fragment>
+
   )
-}
+})
 
 export default QuizOver
